67583274a12b3fcd7e7e5f4fe8468d25b5ab010b
Shisham Chudal <shishamchudal61@gmail.com>
2025-04-15 15:59:15 -0700

    Add helmet middleware for enhanced security

--- express-helmet-app/app.js
+++ express-helmet-app/app.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
+
+app.use(helmet());
 
 app.get('/', (req, res) => {
     console.log('route \'/\' called');
