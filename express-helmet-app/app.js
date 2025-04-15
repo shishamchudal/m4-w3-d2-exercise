b0aba2e4ada5343abaa7efd395d415c2bd439927
Shisham Chudal <shishamchudal61@gmail.com>
2025-04-15 15:56:35 -0700

    Initialize express application with basic routing and server setup

---
+++ express-helmet-app/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log('route \'/\' called');
+    res.send({res: 'Sending 200 as response'});
+});
+
+app.listen(3000);
+console.log('Express app listening on port 3000');
